247453c23cb932c197a37dba14ab6a50a88f7001
Tazim212 <iftekhartazim17@gmail.com>
2026-05-06 23:01:11 +0600

    recent product section done

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,4 +1,6 @@
 import React, { use } from 'react';
+import ProductsCard from './ProductsCard';
+import { Link } from 'react-router';
 const products = fetch("http://localhost:5000/product").then(res =>res.json())
 const Products = () => {
 
@@ -8,6 +10,14 @@ const Products = () => {
     return (
         <div className='my-5'>
             <h1 className='font-bold text-3xl text-center'>Recent Products</h1>
+            <div className='grid grid-cols-1 md:grid-cols-3 mt-3 gap-6'>
+                {
+                    productData.map(product => <ProductsCard key={product._id} product={product}></ProductsCard>)
+                }
+            </div>
+            <div className='text-center'>
+                <Link to="/allproducts"><button className='btn btn-soft btn-secondary my-5'>Show All</button></Link>
+            </div>
         </div>
     );
 };
