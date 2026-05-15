a2dccb1acc32dee7fa34cb9b418a00fb4fd3536f
Tazim212 <iftekhartazim17@gmail.com>
2026-05-15 16:07:00 +0600

    user signin & signup error shown doen

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -7,6 +7,7 @@ import Swal from 'sweetalert2';
 const Register = () => {
     const { handleSignUp, updateUser } = use(AuthContext)
     const [show, setShow] = useState(false)
+    const [error, setError] = useState("")
 
     const location = useLocation()
     const navigate = useNavigate()
@@ -17,9 +18,10 @@ const Register = () => {
         const email = e.target.email.value;
         const password = e.target.password.value
 
+        setError("")
         handleSignUp(email, password)
-            .then(res => {
-                console.log(res.user)
+            .then(() => {
+                // console.log(res.user)
                 const userInfo = {
                     name,
                     email
@@ -28,6 +30,7 @@ const Register = () => {
                     .then(() => { })
                     .catch(err => {
                         console.log(err)
+                        // setError(err)
                     })
                 fetch("http://localhost:5000/user", {
                     method: "POST",
@@ -46,10 +49,11 @@ const Register = () => {
                     })
 
             })
-            .then(err => {
-                console.log(err)
+            .catch(err => {
+                setError(err)
             })
     }
+
     return (
         <form onSubmit={handleRegister}>
             <div className="card bg-base-100 w-full max-w-sm mx-auto my-4 shrink-0 shadow-2xl">
@@ -57,11 +61,11 @@ const Register = () => {
                     <h1 className="text-2xl font-bold">Register now!!!</h1>
                     <fieldset className="fieldset relative">
                         <label className="label">Your Name</label>
-                        <input type="text" className="input" name="name" placeholder="Enter Your Name" />
+                        <input type="text" className="input" name="name" placeholder="Enter Your Name" required/>
                         <label className="label">Email</label>
-                        <input type="email" className="input" name="email" placeholder="Email" />
+                        <input type="email" className="input" name="email" placeholder="Email" required/>
                         <label className="label">Password</label>
-                        <input type={show ? "text" : "password"} className="input" name="password" placeholder="Password" />
+                        <input type={show ? "text" : "password"} className="input" name="password" placeholder="Password" required/>
                         {
                             show ?
                                 <FaEye onClick={() => setShow(false)} className='absolute right-5 top-46'></FaEye>
@@ -72,6 +76,7 @@ const Register = () => {
                         <button className="btn btn-neutral mt-4">Register</button>
                     </fieldset>
                     <p className='text-md italic'>Already have an account? <Link to="/signin" className='underline'>Log in</Link> now</p>
+                    <p className='py-3 text-md text-red-600 text-center'>{error.message}</p>
                 </div>
             </div>
         </form>
