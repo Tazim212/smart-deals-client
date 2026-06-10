f426944280f603a7e8ff987972027e0f9acf43b4
Tazim212 <iftekhartazim21@gmail.com>
2026-06-10 21:28:50 +0600

    remove show all button

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -16,9 +16,9 @@ const Products = () => {
                     productData.map(product => <ProductsCard key={product._id} product={product}></ProductsCard>)
                 }
             </div>
-            <div className='text-center'>
+            {/* <div className='text-center'>
                 <Link to="/allproducts"><button className='btn btn-soft btn-secondary my-5'>Show All</button></Link>
-            </div>
+            </div> */}
         </div>
     );
 };
